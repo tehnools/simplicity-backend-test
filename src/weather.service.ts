ad1dd7b271a869ecbd34c1bae48a253a029a9215
tehnools <noelsoongdev@gmail.com>
2025-09-06 18:06:12 +1200

    task: add tests

--- src/weather.service.ts
+++ src/weather.service.ts
@@ -1,7 +1,5 @@
 const FORCAST_API_URL = "https://api.open-meteo.com/v1/forecast";
 
-//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m
-
 export interface WeatherApiOptions {
   currentWeather?: boolean;
   hourly?: string[];
